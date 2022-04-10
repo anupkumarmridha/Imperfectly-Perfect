b31b72c614d651d2655bd6c9039e5332bb127746
Anup Kumar Mridha <anupkumarmridha.net@gmail.com>
2022-04-10 12:31:21 +0530

    fix product details bugs & updated database

--- home/views.py
+++ home/views.py
@@ -93,16 +93,16 @@ def all_product_details(request):
     all_products=Product.objects.all()
     # context['all_products']=all_products
     product_id=request.GET.get('product_id')
-    # for i in all_products:
-    #     i.order=False
-    #     i.save()
+    for i in all_products:
+        i.order=False
+        i.save()
 
 
     # all_bids=Bid.objects.all()
     cats_menu=Category.objects.all()
-    for i in cats_menu:
-        print(i)
-    print(product_id)
+    # for i in cats_menu:
+    #     print(i)
+    # print(product_id)
     context={
         'all_products':all_products,
         'cats_menu': cats_menu,
